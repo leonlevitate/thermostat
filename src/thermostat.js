23bcac3a685619b68835909ccc27b36e7bb45457
Leon Linton <leon.linton@live.co.uk>
2019-06-27 15:04:46 +0100

    Power Saving mode can be switched on and off, with Tests passing

--- src/thermostat.js
+++ src/thermostat.js
@@ -28,3 +28,11 @@ Thermostat.prototype.isMinimumTemperature = function() {
 Thermostat.prototype.isPowerSavingModeOn = function() {
     return this.powerSavingMode === true;
 }
+
+Thermostat.prototype.switchPowerSavingModeOff = function() {
+    return this.powerSavingMode = false;
+}
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
+    return this.powerSavingMode = true;
+}
