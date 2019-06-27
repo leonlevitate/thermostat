7c1eea6e281b9e5fad308c8c0e4119016fa0c1e4
Leon Linton <leon.linton@live.co.uk>
2019-06-27 17:09:54 +0100

    Displays Usage Levels, with Tests passing

--- src/thermostat.js
+++ src/thermostat.js
@@ -4,9 +4,20 @@ function Thermostat() {
     this.DEFAULT_TEMPERATURE = 20;
     this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
     this.MINIMUM_TEMPERATURE = 10;
     this.MAX_LIMIT_PSM_ON = 25
     this.MAX_LIMIT_PSM_OFF = 32;
+}
+
+Thermostat.prototype.energyUsage = function() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+        return 'Low-usage';
+    }
+    if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+        return 'Medium-usage';
+    }
+        return 'High-usage'; 
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
